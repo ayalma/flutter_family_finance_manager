e48a73d60e90bcaf4aa429abb70fe5acf8fc417c
ayalma(Ali Mohammadi) <alimohammadi7117@gmail.com>
2019-10-01 11:05:52 -0700

    Added Permission request for reading and receiving sms

--- flutter_sms_plugin/android/src/main/kotlin/com/ayalma/flutter_sms_plugin/FlutterSmsPlugin.kt
+++ flutter_sms_plugin/android/src/main/kotlin/com/ayalma/flutter_sms_plugin/FlutterSmsPlugin.kt
@@ -1,25 +1,36 @@
 package com.ayalma.flutter_sms_plugin
 
+import android.Manifest
 import android.app.Activity
 import android.content.BroadcastReceiver
 import android.content.Context
 import android.content.Intent
+import android.content.pm.PackageManager
+import androidx.core.app.ActivityCompat
 import io.flutter.plugin.common.MethodCall
 import io.flutter.plugin.common.MethodChannel
 import io.flutter.plugin.common.MethodChannel.MethodCallHandler
 import io.flutter.plugin.common.MethodChannel.Result
 import io.flutter.plugin.common.PluginRegistry.Registrar
+import io.flutter.plugin.common.PluginRegistry
 
-class FlutterSmsPlugin(registrar: Registrar) : MethodCallHandler, BroadcastReceiver() {
+class FlutterSmsPlugin(registrar: Registrar) : MethodCallHandler, BroadcastReceiver(),PluginRegistry.RequestPermissionsResultListener {
   var activity: Activity = registrar.activity()
   var channel =  MethodChannel(registrar.messenger(), CHANEL_NAME)
 
+  init {
+    channel.setMethodCallHandler(this)
+    registrar.addRequestPermissionsResultListener(this)
+  }
   override fun onReceive(p0: Context?, p1: Intent?) {
     channel.invokeMethod("readSms","test");
+
+
   }
 
   companion object {
     private const val CHANEL_NAME = "flutter_sms_plugin"
+    private const val PERMISSION_REQUEST_CODE = 10001;
     @JvmStatic
     fun registerWith(registrar: Registrar) {
       val smsPlugin = FlutterSmsPlugin(registrar)
@@ -28,9 +39,30 @@ class FlutterSmsPlugin(registrar: Registrar) : MethodCallHandler, BroadcastRecei
 
   override fun onMethodCall(call: MethodCall, result: Result) {
     if (call.method == "getPlatformVersion") {
+      requestPermission()
       result.success("Android ${android.os.Build.VERSION.RELEASE}")
     } else {
       result.notImplemented()
     }
   }
+
+  private fun requestPermission(){
+      val permissions = arrayOf(Manifest.permission.READ_SMS,Manifest.permission.RECEIVE_SMS)
+      ActivityCompat.requestPermissions(activity,permissions, PERMISSION_REQUEST_CODE)
+  }
+
+  override fun onRequestPermissionsResult(requestCode: Int, permissions: Array<out String>?, grantResults: IntArray?): Boolean {
+    if(requestCode == PERMISSION_REQUEST_CODE)
+    {
+        grantResults?.let { results ->
+            if (results.size == 2 && results.all { it == PackageManager.PERMISSION_GRANTED }) {
+
+                return true
+            }
+        }
+    }
+    return false
+  }
+
+
 }
